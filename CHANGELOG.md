39117d67fbea255aeb8186d8a9520139a2ac9ff6
Matteo Franci <mttfranci@gmail.com>
2024-03-24 18:02:02 +0100

    metodo DataService.save( InputStream, String ) implementato.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Changed
 
+- metodo DataService.save( InputStream, String ) implementato.
 - fj-service-helper-version 0.3.2
 
 ## [0.1.0] - 2024-03-23
--- src/main/java/io/github/caffetteria/data/service/opencmis/OpencmisDataService.java
+++ src/main/java/io/github/caffetteria/data/service/opencmis/OpencmisDataService.java
@@ -9,21 +9,62 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
+/**
+ * DataService interface implementation based on a CMIS system.
+ *
+ */
 public class OpencmisDataService implements DataService {
 
     private OpencmisDataServiceFacade facade;
 
+    /**
+     * Load a data stream from the CMIS.
+     *
+     * @param id            the id of the resource to be loaded
+     * @return              the loaded resource data stream
+     * @throws IOException  if Input/Output issues arise
+     */
     @Override
     public InputStream load(String id) throws IOException {
         return new ByteArrayInputStream(this.facade.load( id ));
     }
 
+
+    /**
+     * Save a data stream in the CMIS, a UUID is generated as resource name.
+     *
+     * @param data          the data stream to be saved
+     * @return              the id of the saved resources
+     * @throws IOException if Input/Output issues arise
+     */
     @Override
     public String save(InputStream data) throws IOException {
-        return this.facade.save(UUID.randomUUID().toString(), StreamIO.readBytes( data ));
+        return this.save( data, UUID.randomUUID().toString() );
     }
 
-    public OpencmisDataService setup(OpencmisDataServiceConfig config ) throws ConfigException {
+    /**
+     * Save a data stream in the CMIS, with the given resource name.
+     *
+     * @param data          the data stream to be saved
+     * @param resourceName  the name of the resource to be saved
+     * @return              the id of the saved resource
+     * @throws IOException if Input/Output issues arise
+     */
+    @Override
+    public String save(InputStream data, String resourceName) throws IOException {
+        return this.facade.save( resourceName, StreamIO.readBytes( data ) );
+    }
+
+    /**
+     * Setup this DataService, based on a OpencmisDataServiceConfig.
+     *
+     * Can be invoked only one on any give instance.
+     *
+     * @param config             the configuration
+     * @return                   the self configured instance
+     * @throws ConfigException   if configuration issues arise
+     */
+    public OpencmisDataService setup( OpencmisDataServiceConfig config ) throws ConfigException {
         if ( this.facade == null ) {
             this.facade = new OpencmisDataServiceFacade( config );
         } else {
@@ -32,6 +73,13 @@ public class OpencmisDataService implements DataService {
         return this;
     }
 
+    /**
+     * DataService factory method, based on a OpencmisDataServiceConfig.
+     *
+     * @param config            the configuration
+     * @return                  the self configured instance
+     * @throws ConfigException  if configuration issues arise
+     */
     public static OpencmisDataService newDataService(OpencmisDataServiceConfig config ) throws ConfigException {
         return new OpencmisDataService().setup( config );
     }
